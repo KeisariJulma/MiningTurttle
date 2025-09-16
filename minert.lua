2ff7b46642ca7a05b9e80193859e5fd016af2157
Antti-Jussi Oksa <50314142+KeisariJulma@users.noreply.github.com>
2025-09-16 14:24:42 +0300

    dsa

--- minert.lua
+++ minert.lua
@@ -1,21 +1,18 @@
-print("Hegsdfgsllo")
-
-
 local slot = {
     fuel = 1,
     torch = 2,
     fill = 3
 }
-local plaseTorch = true
-
-
+local placeTorch = true
 
 function Main()
+    turtle.select(slot.fuel)
+    turtle.refuel(1) -- make sure turtle has fuel
     Forward(5)
 end
 
 function Torch()
-    if plaseTorch then
+    if placeTorch then
         turtle.select(slot.torch)
         turtle.place()
     end
@@ -23,11 +20,14 @@ end
 
 function Forward(length)
     print("Forward")
-    for i=1, length, 1 do
-        turtle.forward()
+    for i = 1, length do
+        Torch()
+        turtle.select(slot.fuel) -- make sure fuel slot is selected
+        while not turtle.forward() do
+            turtle.dig()
+            sleep(0.5)
+        end
     end
 end
-
-
 
 Main()
