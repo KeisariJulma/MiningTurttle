af060327a12c7834339048de50225edffb1addce
Antti-Jussi Oksa <50314142+KeisariJulma@users.noreply.github.com>
2025-09-16 14:04:24 +0300

    fds

--- minert.lua
+++ minert.lua
@@ -1,1 +1,23 @@
 print("Hello")
+
+
+local slot = {
+    fuel = 1,
+    torch = 2,
+    fill = 3
+}
+local plaseTorch = true
+
+
+
+function Main()
+    torch()
+    
+end
+
+function torch()
+    if plaseTorch then 
+        turtle.select(slot.torch)
+        turtle.place()
+    end
+end
